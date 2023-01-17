7056f9017ab0722a4eb586a812e6333f8afc4f65
Sandrasangithub <116434357+Sandrasangithub@users.noreply.github.com>
2023-01-17 17:31:21 +0100

    prueba de branch

--- scripts/pruebas.js
+++ scripts/pruebas.js
@@ -1,1 +1,1 @@
-window.location.href = "https://carlos.sanchezdonate.com/redireccion-301-desde-sitebuilders/";
+//window.location.href = "https://carlos.sanchezdonate.com/redireccion-301-desde-sitebuilders/";
