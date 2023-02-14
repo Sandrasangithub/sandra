2a13efdc15181f3f9947a5b2f58de9c778c1744d
Sandrasangithub <116434357+Sandrasangithub@users.noreply.github.com>
2023-02-14 17:08:24 +0100

    Ejercicio del módulo 09 Metaetiquetas

---
+++ unica.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<title>Title de la página única</title>
+<meta name="description" content="A la etiqueta footer le falta la barra para cerrarla">
+<meta name="robots" content="all, max-snippet:25, noimageindex unavaiable_after: 2033-09-27">
+<meta http-equiv="refresh" content="30; url=https://www.google.com/" />
+
+<!-- Open Graph / Facebook 
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://ejemplo.test/unica/">
+<meta property="og:title" content="Title de la página única">
+<meta property="og:description" content="A la etiqueta footer le falta la barra para cerrarla">
+<meta property="og:image" content="https://ejemplo.test//imagenes/perrito.png">
+<meta property="og:image:secure_url" content="https://ejemplo.test//imagenes/perrito.png">
+<meta property="og:image:alt" content="algo bonito"> -->
+
+<!-- Twitter 
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://ejemplo.test/unica/">
+<meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
+<meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+<meta property="twitter:image" content="https://ejemplo.test//imagenes/perrito.png">
+<meta property="twitter:site" content="xxx">
+<meta property="twitter:creator" content="xxx">-->
+
+</head>
+<body>
+    <header>
+        <nav></nav>
+        <h1>Página única</h1>
+    </header>
+        <div data-nosnippet>Elemento con atributo data-nonsnippet, evita que sea mostrado en la SERP de Google porque no nos interesa, puede ser por tratarse de contenido irrelebante, repetitivo, un cta, etc.
+        </div>
+<footer>
+</footer>
+
+</body>
