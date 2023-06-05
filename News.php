4f667f854e6d3a7e61c23c30f777196db89e6238
Sandrasangithub <116434357+Sandrasangithub@users.noreply.github.com>
2023-06-05 20:00:09 +0200

    sitemap-index

---
+++ News.php
@@ -0,0 +1,14 @@
+<?php
+define("pagina", "basica");
+$ciudad = "Salamanca";
+$h1= "<h1>News</h1>" ;
+include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php';
+
+?>
+
+
+
+      <?php
+      include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php';
+
+     ?>
