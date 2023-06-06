8e9df6152cbbb3bc1b64d1aa19b19b45a6585d8e
Sandrasangithub <116434357+Sandrasangithub@users.noreply.github.com>
2023-06-06 21:39:22 +0200

    Plugin con plantilla de sitemap

---
+++ wp-content/plugins/clase-sitemap/test-sitemap.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Lección de Sitemap
+Plugin URI:
+Description: Ejercicio para la clase.
+Author: Sandra
+Author URI: https://carlos.sanchezdonate.com
+Version: 0.1
+License: GPLv2
+*/
+
+//Borrar el sitemap nativo de Wordpress: 
+include 'includes/del-sitemap.php';
+
+//Generar plantilla
+include 'includes/template-generator.php';
+
+?>
