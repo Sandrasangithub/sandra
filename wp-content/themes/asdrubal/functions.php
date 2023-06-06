89924834f7085b6f21551c6acaf716f4bda7c890
Sandrasangithub <116434357+Sandrasangithub@users.noreply.github.com>
2023-06-06 20:29:51 +0200

    eliminar sitemap por defecto

--- wp-content/themes/asdrubal/functions.php
+++ wp-content/themes/asdrubal/functions.php
@@ -10,5 +10,11 @@ include 'agrupapost/agrupapost.php';
 //include $agrupapost . 'agrupapost.php';
 }
     add_shortcode('lastest_post', 'agrupapost');
+
+    // Borrar Sitemap por defecto
+add_filter( 'wp_sitemaps_enabled', '__return_false' );
+if (has_action('init', 'wp_sitemaps_get_server')){
+remove_action('init', 'wp_sitemaps_get_server');
+}
     
 ?>
