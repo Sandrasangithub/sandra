3cad4d9bbbde3cb01890e5cfcbc25294f443f3c1
Sandrasangithub <116434357+Sandrasangithub@users.noreply.github.com>
2023-07-25 20:28:41 +0200

    ejerciciowpo

---
+++ csswpo/jswpo.js
@@ -0,0 +1,47 @@
+<script type='text/javascript' id='sib-front-js-js-extra'>
+var sibErrMsg = {
+    "invalidMail": "Please fill out valid email address",
+    "requiredField": "Please fill out required fields",
+    "invalidDateFormat": "Please fill out valid date format",
+    "invalidSMSFormat": "Please fill out valid phone number"
+};
+var ajax_sib_front_object = {
+    "ajax_url": "https:\/\/sanchezdonate.test\/wp-admin\/admin-ajax.php",
+    "ajax_nonce": "04fcdd2927",
+    "flag_url": "https:\/\/sanchezdonate.test\/wp-content\/plugins\/mailin\/img\/flags\/"
+};
+</script>
+ document.createElement("picture");
+ if (!window.HTMLPictureElement && document.addEventListener) {
+     window.addEventListener("DOMContentLoaded", function() {
+         var s = document.createElement("script");
+         s.src = "<?php echo $url;?>wp-content/plugins/webp-express/js/picturefill.min.js";
+         document.body.appendChild(s);
+     });
+ }
+ <script id="contador">
+ var countDownDate = new Date("Oct 14, 2023 16:00:00").getTime(),
+x = setInterval(function () {
+var $ = new Date().getTime(),
+ o = countDownDate - $;
+(document.getElementById("countdowncarlos").innerHTML =
+ "Comenzamos en: <time>" + Math.floor(o / 864e5) + "d " + Math.floor((o % 864e5) / 36e5) + "h " + Math.floor((o % 36e5) / 6e4) + "m " + Math.floor((o % 6e4) / 1e3) + "s </time>"),
+ o < 0 && (clearInterval(x), (document.getElementById("countdowncarlos").innerHTML = "\xa1Ha comenzado la formaci\xf3n!"));
+}, 1e3);
+
+</script>
+
+ <script id="navbar">
+ function openmenu() {
+             document.getElementById("mobile-nab").className += " expand-menu", document.getElementById("menu-togle").setAttribute("onClick", "closemenu()")
+         }
+
+         function closemenu() {
+             document.getElementById("mobile-nab").className = document.getElementById("mobile-nab").className.replace(/(?:^|\s)expand-menu(?!\S)/g, ""), document.getElementById("menu-togle").setAttribute("onClick", "openmenu()")
+         }
+         $(window).scroll(function() {
+             var e = $(".navbar-header");
+             $(window).scrollTop() >= 30 ? e.addClass("fixed-navbar") : e.removeClass("fixed-navbar")
+         });
+</script>
+
